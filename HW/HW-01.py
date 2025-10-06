b2f5429626365ae206e5ebcebad37937b505700d
Supakorn68 <s6806022510459@email.kmutnb.ac.th>
2025-10-06 15:25:34 +0700

    Delete HW directory

--- HW/HW-01.py
+++
@@ -1,6 +0,0 @@
-def format_strings(*args):
-    return "".join(args).replace(" ", "-").upper
-
-print(format_strings("Hello", "world", "this", "is", "a", "test"))
-print(format_strings("Python", "is", "fun"))
-print(format_strings("Concatenate", "these", "these", "strings", "please"))
