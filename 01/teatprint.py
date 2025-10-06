19fe8bb7d45d3c4a5d57ec86bc5146175bb6898d
Supakorn68 <s6806022510459@email.kmutnb.ac.th>
2025-10-06 15:24:33 +0700

    Delete 01 directory

--- 01/teatprint.py
+++
@@ -1,9 +0,0 @@
-#print("Hello world")
-#print("Supakorn Waenpetch")
-#print("i'm hungry")
-#print("i'm good and \"I will do my best\"")
-#print("/\\")
-print('\tM\nM\tM\tM\n\tM')
-print('''\tM
-M\tM\tM
-\tM''')
